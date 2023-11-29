ef83d701163cf2272d10b62d887f85be306a3048
MariaDelina <mdtrentinarl@gmail.com>
2023-11-29 08:01:06 -0300

    cambios, todo en un mismo archivo

--- conexion.php
+++ conexion.php
@@ -34,7 +34,39 @@ class ConexionCleverCloud {
         header("Access-Control-Allow-Origin: *"); // Permitir solicitudes desde cualquier origen
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); // Métodos permitidos
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"); // Encabezados permitidos
-        header("Referrer-Policy: strict-origin-when-cross-origin"); 
+    }
+
+    public function listarEmpleados() {
+        $consulta = "SELECT * FROM empleados";
+        $stmt = $this->conexion->prepare($consulta);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function crearEmpleado($nombre, $apellido, $sueldo) {
+        $consulta = "INSERT INTO empleados (nombre, apellido, sueldo) VALUES (:nombre, :apellido, :sueldo)";
+        $stmt = $this->conexion->prepare($consulta);
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':apellido', $apellido);
+        $stmt->bindParam(':sueldo', $sueldo);
+        $stmt->execute();
+    }
+
+    public function actualizarEmpleado($id, $nombre, $apellido, $sueldo) {
+        $consulta = "UPDATE empleados SET nombre = :nombre, apellido = :apellido, sueldo = :sueldo WHERE id = :id";
+        $stmt = $this->conexion->prepare($consulta);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':apellido', $apellido);
+        $stmt->bindParam(':sueldo', $sueldo);
+        $stmt->execute();
+    }
+
+    public function eliminarEmpleado($id) {
+        $consulta = "DELETE FROM empleados WHERE id = :id";
+        $stmt = $this->conexion->prepare($consulta);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
     }
 
     // Resto de tu código...
@@ -42,10 +74,3 @@ class ConexionCleverCloud {
 
 // Crear una instancia de la clase
 $conexionCleverCloud = new ConexionCleverCloud();
-
-// Hacer lo que necesites con la conexión...
-
-// No olvides cerrar la conexión cuando hayas terminado
-// $conexionCleverCloud->cerrarConexion();
-
-?>
